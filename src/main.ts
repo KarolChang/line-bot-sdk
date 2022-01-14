7b3d6ed2989f3309f49b98a9fc81303c29fb4a3c
KarolChang <super000999888@gmail.com>
2022-01-14 21:52:30 +0800

    modify push api

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,42 @@
-const line = require('@line/bot-sdk')
-const express = require('express')
-const bodyParser = require('body-parser')
-const crypto = require('crypto')
-// const cors = require('cors')
+import {
+  Client,
+  middleware,
+  JSONParseError,
+  SignatureValidationFailed,
+  ClientConfig,
+  MiddlewareConfig,
+  TextMessage,
+  WebhookEvent
+} from '@line/bot-sdk'
+import * as express from 'express'
+import { Request, Response } from 'express'
+import * as bodyParser from 'body-parser'
+import * as crypto from 'crypto'
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 // create LINE SDK config from env variables
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+const config: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN!,
   channelSecret: process.env.CHANNEL_SECRET
 }
 
 // create LINE SDK client
-const client = new line.Client(config)
+const client = new Client(config)
 // express app
 const app = express()
 
 // webhook
-app.post('/callback', line.middleware(config), async (req, res) => {
+app.post('/callback', middleware(config as MiddlewareConfig), async (req: Request, res: Response) => {
   console.log('req.body.events', req.body.events)
   // 給 LINE 的 body 要是 string
   const body = JSON.stringify(req.body)
   // 取得 LINE 的簽名
-  const signature = crypto.createHmac('SHA256', process.env.CHANNEL_SECRET).update(body).digest('base64')
+  const signature = crypto
+    .createHmac('SHA256', process.env.CHANNEL_SECRET as string)
+    .update(body)
+    .digest('base64')
   // 取得 headers 中的 X-Line-Signature
   const headerX = req.get('X-Line-Signature')
   // 比對 signature, headers ，二者相等時才代表是由 LINE server 發來的訊息
@@ -45,40 +57,36 @@ app.use(bodyParser.json())
 
 // push message
 app.post('/push', async (req, res) => {
-  console.log('req.body', req.body)
   const { to, messages } = req.body
   if (typeof to === 'string') {
     await client.pushMessage(to, messages)
-    console.log('pushMessage')
     return res.json({ status: 'success push one', to, messages })
   } else if (typeof to === 'object' && to.length) {
     await client.multicast(to, messages)
-    console.log('multicast')
     return res.json({ status: 'success push many', to, messages })
   } else {
-    console.log('wrong input')
     return res.json({ status: 'error', message: 'wrong input!!!' })
   }
 })
 
 // event handler
-function handleReply(event) {
+function handleReply(event: WebhookEvent) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null)
   }
   // create a echoing text message
-  const echo = { type: 'text', text: `促咪卡比說：${event.message.text}` }
+  const echo: TextMessage = { type: 'text', text: `促咪卡比說：${event.message.text}` }
   // use reply API
   client.replyMessage(event.replyToken, echo)
 }
 
 // error handling
-app.use((err, req, res, next) => {
-  if (err instanceof line.SignatureValidationFailed) {
+app.use((err, req: Request, res: Response, next) => {
+  if (err instanceof SignatureValidationFailed) {
     res.status(401).send(err.signature)
     return
-  } else if (err instanceof line.JSONParseError) {
+  } else if (err instanceof JSONParseError) {
     res.status(400).send(err.raw)
     return
   }
@@ -90,6 +98,3 @@ const port = process.env.PORT
 app.listen(port, () => {
   console.log(`line bot is listening on http://localhost:${port}`)
 })
-
-// 文件
-// https://line.github.io/line-bot-sdk-nodejs/guide/client.html#create-a-client
