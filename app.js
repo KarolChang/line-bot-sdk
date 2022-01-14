7af185bae9a1df330afe2396476ff8929aad6ef6
KarolChang <super000999888@gmail.com>
2022-01-14 14:30:25 +0800

    deploy

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const line = require('@line/bot-sdk')
+// import { Client, JSONParseError, SignatureValidationFailed, middleware } from '@line/bot-sdk'
 const express = require('express')
+const crypto = require('crypto')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -16,17 +18,33 @@ const client = new line.Client(config)
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express()
+app.use(line.middleware(config))
+// app.get('/', (req, res) => {
+//   res.send('hello')
+// })
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
+app.post('/callback', (req, res) => {
   console.log('req.body.events', req.body.events)
-  Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err)
-      res.status(500).end()
-    })
+  // 給 LINE 的 body 要是 string
+  const body = JSON.stringify(req.body)
+
+  // 取得 LINE 的簽名
+  const signature = crypto.createHmac('SHA256', channelSecret).update(body).digest('base64')
+
+  // 取得 headers 中的 X-Line-Signature
+  const headerX = req.get('X-Line-Signature')
+
+  // 比對 signature, headers ，二者相等時才代表是由 LINE server 發來的訊息
+  if (signature === headerX) {
+    Promise.all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err)
+        res.status(500).end()
+      })
+  }
 })
 
 // event handler
@@ -43,8 +61,20 @@ function handleEvent(event) {
   return client.replyMessage(event.replyToken, echo)
 }
 
+// error handling
+// app.use((err, req, res, next) => {
+//   if (err instanceof line.SignatureValidationFailed) {
+//     res.status(401).send(err.signature)
+//     return
+//   } else if (err instanceof line.JSONParseError) {
+//     res.status(400).send(err.raw)
+//     return
+//   }
+//   next(err) // will throw default 500
+// })
+
 // listen on port
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.listen(port, () => {
-  console.log(`listening on ${port}`)
+  console.log(`line bot is listening on http://localhost:${port}`)
 })
