4d4c4046cb34c7bbd279962deb9e9339ea774784
KarolChang <super000999888@gmail.com>
2022-01-14 15:08:23 +0800

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const line = require('@line/bot-sdk')
-// import { Client, JSONParseError, SignatureValidationFailed, middleware } from '@line/bot-sdk'
 const express = require('express')
+const bodyParser = require('body-parser')
 const crypto = require('crypto')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -14,42 +14,37 @@ const config = {
 
 // create LINE SDK client
 const client = new line.Client(config)
-
-// create Express app
-// about Express itself: https://expressjs.com/
+// express app
 const app = express()
-app.use(line.middleware(config))
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', async (req, res) => {
+// webhook
+app.post('/callback', line.middleware(config), async (req, res) => {
   console.log('req.body.events', req.body.events)
   // 給 LINE 的 body 要是 string
   const body = JSON.stringify(req.body)
-
   // 取得 LINE 的簽名
   const signature = crypto.createHmac('SHA256', process.env.CHANNEL_SECRET).update(body).digest('base64')
-
   // 取得 headers 中的 X-Line-Signature
   const headerX = req.get('X-Line-Signature')
-
   // 比對 signature, headers ，二者相等時才代表是由 LINE server 發來的訊息
   if (signature === headerX) {
     try {
-      // await handleReply(req.body.events[0])
-      await handlePush()
-      await client.pushMessage(process.env.KAROL_USERID, { type: 'text', text: '促咪卡比1' })
+      await handleReply(req.body.events[0])
     } catch (err) {
       console.log('[ERROR]', err)
       res.status(500).end()
     }
-    // Promise.all(req.body.events.map(handleEvent))
-    //   .then((result) => res.json(result))
-    //   .catch((err) => {
-    //     console.error(err)
-    //     res.status(500).end()
-    //   })
   }
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/push', async (req, res) => {
+  const message = req.body.message
+  const inputMsg = { type: 'text', text: message }
+  await client.pushMessage(process.env.KAROL_USERID, inputMsg)
+  return res.json({ status: 'success', inputMsg })
 })
 
 // event handler
@@ -58,30 +53,23 @@ function handleReply(event) {
     // ignore non-text-message event
     return Promise.resolve(null)
   }
-
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text }
-  console.log('event.message', event.message)
   // use reply API
   client.replyMessage(event.replyToken, echo)
 }
 
-function handlePush() {
-  client.pushMessage(process.env.KAROL_USERID, { type: 'text', text: '促咪卡比' })
-  return Promise.resolve(null)
-}
-
 // error handling
-// app.use((err, req, res, next) => {
-//   if (err instanceof line.SignatureValidationFailed) {
-//     res.status(401).send(err.signature)
-//     return
-//   } else if (err instanceof line.JSONParseError) {
-//     res.status(400).send(err.raw)
-//     return
-//   }
-//   next(err) // will throw default 500
-// })
+app.use((err, req, res, next) => {
+  if (err instanceof line.SignatureValidationFailed) {
+    res.status(401).send(err.signature)
+    return
+  } else if (err instanceof line.JSONParseError) {
+    res.status(400).send(err.raw)
+    return
+  }
+  next(err) // will throw default 500
+})
 
 // listen on port
 const port = process.env.PORT
